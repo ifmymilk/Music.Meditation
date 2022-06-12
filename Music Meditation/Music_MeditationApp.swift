d7c20efc0a230870e0c5f515d65f7b65f788da9a
ifmymilk <59724526+ifmymilk@users.noreply.github.com>
2022-06-12 10:26:41 +0200

    Initial Commit

---
+++ Music Meditation/Music_MeditationApp.swift
@@ -0,0 +1,17 @@
+//
+//  Music_MeditationApp.swift
+//  Music Meditation
+//
+//  Created by Simon LE on 12/06/2022.
+//
+
+import SwiftUI
+
+@main
+struct Music_MeditationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
